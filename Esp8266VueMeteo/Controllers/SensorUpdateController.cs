cc603f242ac8b7ff1d7812c68d7e86b9b535b259
Piotr Kozak <piotr.kozak@pirios.com>
2019-11-04 13:56:16 +0100

    - simple request

---
+++ Esp8266VueMeteo/Controllers/SensorUpdateController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Esp8266VueMeteo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Esp8266VueMeteo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorUpdateController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("OK!");
+        }
+
+        [HttpPost]
+        public IActionResult UpdateData([FromBody]SensorUpdateRequest request)
+        {
+            return new JsonResult(request);
+        }
+    }
+}
---
+++ Esp8266VueMeteo/Models/SensorUpdateRequest.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Esp8266VueMeteo.Models
+{
+    public class SensorUpdateRequest
+    {
+        [JsonProperty(PropertyName = "esp8266id")]
+        public string EspId { get; set; }
+        [JsonProperty(PropertyName = "sensordatavalues")]
+        public IList<SensorData> DataValues { get; set; }
+
+        public SensorUpdateRequest()
+        {
+            DataValues = new List<SensorData>();
+        }
+    }
+
+    public class SensorData
+    {
+        [JsonProperty(PropertyName = "value_type")]
+        public string ValueType { get; set; }
+        [JsonProperty(PropertyName = "value")]
+        public string Value { get; set; }
+    }
+}
